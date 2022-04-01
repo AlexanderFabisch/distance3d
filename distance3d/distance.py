4858d2c60678e5b201ad8aae330c264ff33afa10
Alexander Fabisch <afabisch@googlemail.com>
2022-04-01 16:11:44 +0200

    Distance from line segment to box

--- distance3d/distance.py
+++ distance3d/distance.py
@@ -984,6 +984,55 @@ def _case_000(point_in_box, box_half_size):
     return np.dot(deltas, deltas)
 
 
+def line_segment_to_box(segment_start, segment_end, box2origin, size, origin2box=None):
+    """Compute the shortest distance from line segment to box.
+
+    Parameters
+    ----------
+    segment_start : array, shape (3,)
+        Start point of segment.
+
+    segment_end : array, shape (3,)
+        End point of segment.
+
+    box2origin : array, shape (4, 4)
+        Pose of the box.
+
+    size : array, shape (3,)
+        Size of the box along its axes.
+
+    origin2box : array, shape (4, 4), optional (default: None)
+        Transform from origin to box coordinates.
+
+    Returns
+    -------
+    distance : float
+        The shortest distance between line segment and box.
+
+    contact_point_line_segment : array, shape (3,)
+        Closest point on line segment.
+
+    contact_point_box : array, shape (3,)
+        Closest point on box.
+    """
+    segment_direction, segment_length = convert_segment_to_line(
+        segment_start, segment_end)
+
+    distance, contact_point_segment, contact_point_box, t_closest = _line_to_box(
+        segment_start, segment_direction, box2origin, size, origin2box=origin2box)
+
+    if t_closest < 0:
+        distance, contact_point_box = point_to_box(
+            segment_start, box2origin, size, origin2box=origin2box)
+        contact_point_segment = segment_start
+    elif t_closest > segment_length:
+        distance, contact_point_box = point_to_box(
+            segment_end, box2origin, size, origin2box=origin2box)
+        contact_point_segment = segment_end
+
+    return distance, contact_point_segment, contact_point_box
+
+
 def point_to_triangle(point, triangle_points):
     """Compute the shortest distance between point and triangle.
 
---
+++ examples/plot_line_segment_to_box.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pytransform3d.transformations as pt
+import pytransform3d.plot_utils as ppu
+from distance3d.distance import line_segment_to_box
+
+
+random_state = np.random.RandomState(2)
+box2origin = np.eye(4)
+size = np.ones(3)
+
+ax = ppu.make_3d_axis(ax_s=2)
+
+for i in range(10):
+    offset = random_state.randn()
+    segment_start = offset + random_state.randn(3) / 2
+    segment_end = offset + random_state.randn(3) / 2
+    dist, contact_point_segment, contact_point_box = line_segment_to_box(
+        segment_start, segment_end, box2origin, size)
+    print(dist)
+    segment = np.vstack((segment_start, segment_end))
+    points = np.vstack((contact_point_segment, contact_point_box))
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    ax.plot(points[:, 0], points[:, 1], points[:, 2])
+    ax.scatter(segment[:, 0], segment[:, 1], segment[:, 2])
+    ax.plot(segment[:, 0], segment[:, 1], segment[:, 2])
+
+ppu.plot_box(ax=ax, A2B=box2origin, size=size, wireframe=False, alpha=0.5)
+pt.plot_transform(ax=ax, A2B=box2origin, s=0.1)
+
+plt.show()
