f7bcee74d4096606a4dcfe578ee91866a71d3d77
Alexander Fabisch <afabisch@googlemail.com>
2022-04-02 11:11:32 +0200

    Distance from point to cylinder

--- distance3d/distance/__init__.py
+++ distance3d/distance/__init__.py
@@ -1036,6 +1036,49 @@ def triangle_to_triangle(triangle_points1, triangle_points2, epsilon=1e-6):
     return best_dist, best_contact_point_triangle1, best_contact_point_triangle2
 
 
+def point_to_cylinder(point, cylinder2origin, radius, length):
+    """Compute the shortest distance between point and cylinder.
+
+    Parameters
+    ----------
+    point : array, shape (3,)
+        3D point.
+
+    cylinder2origin : array, shape (4, 4)
+        Pose of the cylinder.
+
+    radius : float
+        Radius of the cylinder.
+
+    length : float
+        Length of the cylinder.
+
+    Returns
+    -------
+    distance : float
+        The shortest distance between point and triangle.
+
+    contact_point : array, shape (3,)
+        Closest point on cylinder.
+    """
+    # signed distance from point to plane of disk
+    diff = point - cylinder2origin[:3, 3]
+    dist_to_plane = diff.dot(cylinder2origin[:3, 2])
+
+    # projection of P - C onto plane is Q - C = P - C - dist_to_plane * N
+    diff_in_plane = diff - dist_to_plane * cylinder2origin[:3, 2]
+    sqr_len = diff_in_plane.dot(diff_in_plane)
+
+    contact_point = (
+        cylinder2origin[:3, 3]
+        + min(1.0, (radius / math.sqrt(sqr_len))) * diff_in_plane
+        + np.clip(dist_to_plane, -0.5 * length, 0.5 * length) * cylinder2origin[:3, 2])
+    contact_point_to_point = point - contact_point
+    sqr_dist = contact_point_to_point.dot(contact_point_to_point)
+
+    return math.sqrt(sqr_dist), contact_point
+
+
 def point_to_ellipsoid(
         point, ellipsoid2origin, radii, epsilon=1e-16, max_iter=64):
     """Compute the shortest distance between point and ellipsoid.
---
+++ examples/plot_point_to_cylinder.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pytransform3d.transformations as pt
+import pytransform3d.plot_utils as ppu
+from distance3d.distance import point_to_cylinder
+from distance3d import random
+
+
+random_state = np.random.RandomState(3)
+cylinder2origin, radius, length = random.rand_cylinder(random_state, min_radius=0.5, min_length=1.0)
+
+ax = ppu.make_3d_axis(ax_s=2)
+
+for i in range(50):
+    point = random_state.randn(3)
+    dist, contact_point = point_to_cylinder(point, cylinder2origin, radius, length)
+    print(dist)
+    points = np.vstack((point, contact_point))
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    ax.plot(points[:, 0], points[:, 1], points[:, 2])
+
+ppu.plot_cylinder(ax=ax, A2B=cylinder2origin, radius=radius, length=length, wireframe=False, alpha=0.5)
+pt.plot_transform(ax=ax, A2B=cylinder2origin, s=0.1)
+
+plt.show()
