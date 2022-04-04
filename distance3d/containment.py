5d2d447481fb484d6920288a62bdb2181559e2cb
Alexander Fabisch <afabisch@googlemail.com>
2022-04-04 21:59:26 +0200

    Precompute arrays

--- distance3d/containment.py
+++ distance3d/containment.py
@@ -32,23 +32,25 @@ def box_aabb(box2origin, size):
     return axis_aligned_bounding_box(vertices)
 
 
+XM = np.array([-1.0, 0.0, 0.0])
+YM = np.array([0.0, -1.0, 0.0])
+ZM = np.array([0.0, 0.0, -1.0])
+XP = np.array([1.0, 0.0, 0.0])
+YP = np.array([0.0, 1.0, 0.0])
+ZP = np.array([0.0, 0.0, 1.0])
+
+
 def cylinder_aabb(cylinder2origin, radius, length):
     negative_vertices = np.vstack((
-        cylinder_extreme_along_direction(
-            np.array([-1.0, 0.0, 0.0]), cylinder2origin, radius, length),
-        cylinder_extreme_along_direction(
-            np.array([0.0, -1.0, 0.0]), cylinder2origin, radius, length),
-        cylinder_extreme_along_direction(
-            np.array([0.0, 0.0, -1.0]), cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(XM, cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(YM, cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(ZM, cylinder2origin, radius, length),
     ))
     mins = np.min(negative_vertices, axis=0)
     positive_vertices = np.vstack((
-        cylinder_extreme_along_direction(
-            np.array([1.0, 0.0, 0.0]), cylinder2origin, radius, length),
-        cylinder_extreme_along_direction(
-            np.array([0.0, 1.0, 0.0]), cylinder2origin, radius, length),
-        cylinder_extreme_along_direction(
-            np.array([0.0, 0.0, 1.0]), cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(XP, cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(YP, cylinder2origin, radius, length),
+        cylinder_extreme_along_direction(ZP, cylinder2origin, radius, length),
     ))
     maxs = np.max(positive_vertices, axis=0)
     return mins, maxs
@@ -56,21 +58,15 @@ def cylinder_aabb(cylinder2origin, radius, length):
 
 def capsule_aabb(capsule2origin, radius, height):
     negative_vertices = np.vstack((
-        capsule_extreme_along_direction(
-            np.array([-1.0, 0.0, 0.0]), capsule2origin, radius, height),
-        capsule_extreme_along_direction(
-            np.array([0.0, -1.0, 0.0]), capsule2origin, radius, height),
-        capsule_extreme_along_direction(
-            np.array([0.0, 0.0, -1.0]), capsule2origin, radius, height),
+        capsule_extreme_along_direction(XM, capsule2origin, radius, height),
+        capsule_extreme_along_direction(YM, capsule2origin, radius, height),
+        capsule_extreme_along_direction(ZM, capsule2origin, radius, height),
     ))
     mins = np.min(negative_vertices, axis=0)
     positive_vertices = np.vstack((
-        capsule_extreme_along_direction(
-            np.array([1.0, 0.0, 0.0]), capsule2origin, radius, height),
-        capsule_extreme_along_direction(
-            np.array([0.0, 1.0, 0.0]), capsule2origin, radius, height),
-        capsule_extreme_along_direction(
-            np.array([0.0, 0.0, 1.0]), capsule2origin, radius, height),
+        capsule_extreme_along_direction(XP, capsule2origin, radius, height),
+        capsule_extreme_along_direction(YP, capsule2origin, radius, height),
+        capsule_extreme_along_direction(ZP, capsule2origin, radius, height),
     ))
     maxs = np.max(positive_vertices, axis=0)
     return mins, maxs
