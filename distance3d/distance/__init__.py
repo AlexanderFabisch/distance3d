f9099153daf6a78a2659429c677f172ca395bfe9
Alexander Fabisch <afabisch@googlemail.com>
2022-04-07 22:11:44 +0200

    API documentation

--- distance3d/distance/__init__.py
+++ distance3d/distance/__init__.py
@@ -1,7 +1,7 @@
 """Distance calculation of specific geometric shapes."""
 from ._line import (
     point_to_line, line_to_line, point_to_line_segment, line_to_line_segment,
-    line_segment_to_line_segment, _line_to_line_segment)
+    line_segment_to_line_segment)
 from ._triangle import (
     point_to_triangle, line_to_triangle, line_segment_to_triangle,
     triangle_to_triangle, triangle_to_rectangle)
@@ -15,3 +15,14 @@ from ._cylinder import point_to_cylinder
 from ._ellipsoid import point_to_ellipsoid
 from ._circle import point_to_circle
 from ._disk import point_to_disk
+
+
+__all__ = ["point_to_line", "line_to_line", "point_to_line_segment",
+           "line_to_line_segment", "line_segment_to_line_segment",
+           "point_to_triangle", "line_to_triangle", "line_segment_to_triangle",
+           "triangle_to_triangle", "triangle_to_rectangle",
+           "point_to_rectangle", "line_to_rectangle",
+           "line_segment_to_rectangle", "rectangle_to_rectangle",
+           "point_to_box", "line_to_box", "line_segment_to_box",
+           "rectangle_to_box", "point_to_plane", "point_to_cylinder",
+           "point_to_ellipsoid", "point_to_circle", "point_to_disk"]
---
+++ doc/source/index.rst
@@ -0,0 +1,21 @@
+.. distance3d documentation master file, created by
+   sphinx-quickstart on Thu Apr  7 20:46:08 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to distance3d's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
