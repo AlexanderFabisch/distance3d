81267250e0a0d8be90d6d62352825c37c3c63348
Alexander Fabisch <afabisch@googlemail.com>
2022-04-08 10:27:45 +0200

    Clean up example

--- examples/distance/plot_line_segment_to_box.py
+++ examples/distance/plot_line_segment_to_box.py
@@ -4,11 +4,12 @@ Distance from line segment to box
 =================================
 """
 print(__doc__)
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-import pytransform3d.transformations as pt
 import pytransform3d.plot_utils as ppu
 from distance3d.distance import line_segment_to_box
+from distance3d import random, plotting
 
 
 random_state = np.random.RandomState(2)
@@ -17,21 +18,20 @@ size = np.ones(3)
 
 ax = ppu.make_3d_axis(ax_s=2)
 
-for i in range(10):
-    offset = random_state.randn()
-    segment_start = offset + random_state.randn(3) / 2
-    segment_end = offset + random_state.randn(3) / 2
+accumulated_time = 0.0
+for i in range(8000):
+    segment_start, segment_end = random.randn_line_segment(random_state)
+    start = time.time()
     dist, contact_point_segment, contact_point_box = line_segment_to_box(
         segment_start, segment_end, box2origin, size)
+    end = time.time()
+    accumulated_time += end - start
     print(dist)
-    segment = np.vstack((segment_start, segment_end))
-    points = np.vstack((contact_point_segment, contact_point_box))
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
-    ax.plot(points[:, 0], points[:, 1], points[:, 2])
-    ax.scatter(segment[:, 0], segment[:, 1], segment[:, 2])
-    ax.plot(segment[:, 0], segment[:, 1], segment[:, 2])
+    if i > 15:
+        continue
+    plotting.plot_segment(ax, contact_point_segment, contact_point_box)
+    plotting.plot_segment(ax, segment_start, segment_end)
+print(f"{accumulated_time=}")
 
 ppu.plot_box(ax=ax, A2B=box2origin, size=size, wireframe=False, alpha=0.5)
-pt.plot_transform(ax=ax, A2B=box2origin, s=0.1)
-
 plt.show()
