ab913d74160975210bd9ce8f909ffc10ce64fb2a
Alexander Fabisch <afabisch@googlemail.com>
2022-04-09 22:08:45 +0200

    Test GJK

---
+++ distance3d/test/test_gjk.py
@@ -0,0 +1,26 @@
+import numpy as np
+from distance3d import colliders, gjk
+from pytest import approx
+
+
+def test_gjk_boxes():
+    box2origin = np.array([
+        [-0.29265666, -0.76990535, 0.56709596, 0.1867558],
+        [0.93923897, -0.12018753, 0.32153556, -0.09772779],
+        [-0.17939408, 0.62673815, 0.75829879, 0.09500884],
+        [0., 0., 0., 1.]])
+    size = np.array([2.89098828, 1.15032456, 2.37517511])
+    box_collider = colliders.Box(box2origin, size)
+
+    box2origin2 = np.array([
+        [-0.29265666, -0.76990535, 0.56709596, 3.73511598],
+        [0.93923897, -0.12018753, 0.32153556, -1.95455576],
+        [-0.17939408, 0.62673815, 0.75829879, 1.90017684],
+        [0., 0., 0., 1.]])
+    size2 = np.array([0.96366276, 0.38344152, 0.79172504])
+
+    box_collider2 = colliders.Box(box2origin2, size2)
+    dist, contact_point_box, contact_point_box2, _ = gjk.gjk_with_simplex(
+        box_collider, box_collider2)
+
+    assert approx(dist) == 1.7900192730149391
