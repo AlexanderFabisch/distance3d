71e02c79758b07e2572de49e882916b9b014906d
Alexander Fabisch <afabisch@googlemail.com>
2022-04-15 23:26:42 +0200

    Compare triangle to triangle with GJK

--- benchmarks/benchmark_distance_triangle_to_triangle.py
+++ benchmarks/benchmark_distance_triangle_to_triangle.py
@@ -2,7 +2,7 @@ import timeit
 from functools import partial
 import numpy as np
 from distance3d.distance import triangle_to_triangle
-from distance3d import random
+from distance3d import random, gjk, geometry
 
 
 random_state = np.random.RandomState(0)
@@ -16,5 +16,20 @@ def distance_random_triangle_to_triangle(random_state, triangle_points):
 
 times = timeit.repeat(partial(
     distance_random_triangle_to_triangle, random_state=random_state,
+    triangle_points=triangle_points), repeat=10, number=200)
+print(f"Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
+
+
+random_state = np.random.RandomState(0)
+triangle_points = random.randn_triangle(random_state) * 0.5
+
+
+def distance_random_triangle_to_triangle_gjk(random_state, triangle_points):
+    triangle_points2 = random.randn_triangle(random_state)
+    gjk.gjk(triangle_points2, triangle_points)
+
+
+times = timeit.repeat(partial(
+    distance_random_triangle_to_triangle_gjk, random_state=random_state,
     triangle_points=triangle_points), repeat=10, number=200)
 print(f"Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
