a7026541c3161f31b37160b12bc1bdcdc2b217a5
Alexander Fabisch <afabisch@googlemail.com>
2022-05-03 10:53:42 +0200

    Refactor EPA

--- distance3d/epa.py
+++ distance3d/epa.py
@@ -84,13 +84,12 @@ def epa(simplex, collider1, collider2, max_iter=64, max_loose_edges=32, max_face
     success : bool
         EPA converged before maximum number of iterations was reached.
     """
-    faces = Faces(simplex, max_faces)
-    loose_edges = LooseEdges(max_loose_edges)
+    polytope = Polytope(simplex, max_faces, epsilon)
+    loose_edges = LooseEdges(max_loose_edges, epsilon)
 
     for iteration in range(max_iter):
-        min_dist, closest_face = faces.find_face_closest_to_origin()
+        min_dist, closest_face = polytope.find_face_closest_to_origin()
 
-        # Search normal to face that is closest to origin
         search_direction = closest_face[3]
         _, new_vertex1 = collider1.support_function(search_direction)
         _, new_vertex2 = collider2.support_function(-search_direction)
@@ -99,41 +98,41 @@ def epa(simplex, collider1, collider2, max_iter=64, max_loose_edges=32, max_face
         if np.dot(new_point, search_direction) - min_dist < epsilon:
             # Convergence: new point is not significantly further from origin.
             mtv = closest_face[3] * np.dot(new_point, search_direction)
-            return mtv, faces.faces[:faces.n_faces], True
+            return mtv, polytope.get_all_faces(), True
 
-        loose_edges.find_loose_edges(faces, new_point, epsilon)
-        faces.extend_polytope(loose_edges, new_point)
+        loose_edges.find_triangles_facing_point_and_store_loose_edges(
+            polytope, new_point)
+        polytope.extend_with_point(loose_edges, new_point)
 
     # Return most recent closest point
     mtv = closest_face[3] * np.dot(closest_face[0], closest_face[3])
-    return mtv, faces.faces[:faces.n_faces], False
+    return mtv, polytope.get_all_faces(), False
 
 
-class Faces:
+class Polytope:
     """Array of faces, each with 3 vertices and a normal."""
-    def __init__(self, simplex, max_faces):
+    def __init__(self, simplex, max_faces, epsilon):
         self.max_faces = max_faces
+        self.epsilon = epsilon
         self.faces = np.zeros((self.max_faces, 4, 3))
         self.n_faces = self._initialize_from_simplex(simplex)
 
     def _initialize_from_simplex(self, simplex):
-        """Initialize with final simplex from GJK."""
-        # ABC
-        self.faces[0, :3] = simplex[:3]
-        self.faces[0, 3] = norm_vector(np.cross(simplex[1] - simplex[0], simplex[2] - simplex[0]))
-        # ACD
-        self.faces[1, :3] = simplex[np.array((0, 2, 3), dtype=int)]
-        self.faces[1, 3] = norm_vector(np.cross(simplex[2] - simplex[0], simplex[3] - simplex[0]))
-        # ADB
-        self.faces[2, :3] = simplex[np.array((0, 3, 1), dtype=int)]
-        self.faces[2, 3] = norm_vector(np.cross(simplex[3] - simplex[0], simplex[1] - simplex[0]))
-        # BDC
-        self.faces[3, :3] = simplex[np.array((1, 3, 2), dtype=int)]
-        self.faces[3, 3] = norm_vector(np.cross(simplex[3] - simplex[1], simplex[2] - simplex[1]))
-        return 4
+        self.faces[0, :3] = simplex[:3]  # ABC
+        self.faces[1, :3] = simplex[np.array((0, 2, 3), dtype=int)]  # ACD
+        self.faces[2, :3] = simplex[np.array((0, 3, 1), dtype=int)]  # ADB
+        self.faces[3, :3] = simplex[np.array((1, 3, 2), dtype=int)]  # BDC
+        n_faces = 4
+        for i in range(n_faces):
+            self.compute_normal(i)
+        return n_faces
+
+    def compute_normal(self, face_idx):
+        self.faces[face_idx, 3] = norm_vector(
+            np.cross(self.faces[face_idx, 1] - self.faces[face_idx, 0],
+                     self.faces[face_idx, 2] - self.faces[face_idx, 0]))
 
     def find_face_closest_to_origin(self):
-        """Find face that is closest to origin."""
         dists = np.sum(
             self.faces[:self.n_faces, 0] * self.faces[:self.n_faces, 3],
             axis=1)
@@ -144,24 +143,25 @@ class Faces:
         return np.vstack([self.faces[face_idx, edge_idx],
                           self.faces[face_idx, (edge_idx + 1) % 3]])
 
+    def get_all_faces(self):
+        return self.faces[:self.n_faces]
+
     def remove_face(self, i):
         self.faces[i] = self.faces[self.n_faces - 1]
         self.n_faces -= 1
 
-    def triangle_faces_point(self, i, new_points, epsilon):
-        return np.dot(self.faces[i, 3], new_points - self.faces[i, 0]) > epsilon
+    def triangle_faces_point(self, i, new_points):
+        return np.dot(self.faces[i, 3],
+                      new_points - self.faces[i, 0]) > self.epsilon
 
-    def extend_polytope(self, loose_edges, new_points):
-        """Reconstruct polytope with p added."""
+    def extend_with_point(self, loose_edges, new_point):
         for i in range(loose_edges.n_loose_edges):
             assert self.n_faces < self.max_faces
             if self.n_faces >= self.max_faces:
                 break
             self.faces[self.n_faces, :2] = loose_edges.loose_edges[i]
-            self.faces[self.n_faces, 2] = new_points
-            self.faces[self.n_faces, 3] = norm_vector(
-                np.cross(self.faces[self.n_faces, 0] - self.faces[self.n_faces, 1],
-                         self.faces[self.n_faces, 0] - self.faces[self.n_faces, 2]))
+            self.faces[self.n_faces, 2] = new_point
+            self.compute_normal(self.n_faces)
             if np.linalg.norm(self.faces[self.n_faces, 3]) < 0.5:  # TODO is this the right solution?
                 continue
             self.fix_ccw_normal_direction(self.n_faces)
@@ -179,29 +179,29 @@ class Faces:
 
 class LooseEdges:
     """Keep track of edges we need to fix after removing faces."""
-    def __init__(self, max_loose_edges):
+    def __init__(self, max_loose_edges, epsilon):
         self.max_loose_edges = max_loose_edges
+        self.epsilon = epsilon
         self.loose_edges = np.zeros((self.max_loose_edges, 2, 3))
         self.n_loose_edges = 0
 
-    def find_loose_edges(self, faces, new_point, epsilon):
-        """Find all triangles that are facing p and store loose edges."""
+    def find_triangles_facing_point_and_store_loose_edges(self, faces, new_point):
         self.n_loose_edges = 0
         i = 0
         while i < faces.n_faces:
-            if faces.triangle_faces_point(i, new_point, epsilon):
-                self.add_removed_triangles_edges_to_list(faces, i, epsilon)
+            if faces.triangle_faces_point(i, new_point):
+                self.add_removed_triangles_edges_to_list(faces, i)
                 faces.remove_face(i)
                 i -= 1
             i += 1
 
-    def add_removed_triangles_edges_to_list(self, faces, i, epsilon):
+    def add_removed_triangles_edges_to_list(self, faces, i):
         for j in range(EDGES_PER_FACE):
             current_edge = faces.get_edge(i, j)
             found_edge = False
             k = 0
             while k < self.n_loose_edges:
-                if self.edge_already_in_list(k, current_edge, epsilon):
+                if self.edge_already_in_list(k, current_edge):
                     # This assumes edge can only be shared by 2 triangles
                     # (which should be true). This also assumes shared edge
                     # will be reversed in the triangles (which should be true
@@ -213,16 +213,20 @@ class LooseEdges:
                 k += 1
 
             if not found_edge:
-                self.add_current_edge_to_list(current_edge)
+                success = self.add_edge_to_list(current_edge)
+                if not success:
+                    break
 
-    def edge_already_in_list(self, edge_idx, current_edge, epsilon):
-        return (np.linalg.norm(self.loose_edges[edge_idx, 1] - current_edge[0]) < epsilon and
-                np.linalg.norm(self.loose_edges[edge_idx, 0] - current_edge[1]) < epsilon)
+    def edge_already_in_list(self, edge_idx, current_edge):
+        return (np.linalg.norm(self.loose_edges[edge_idx, 1] - current_edge[0]) < self.epsilon and
+                np.linalg.norm(self.loose_edges[edge_idx, 0] - current_edge[1]) < self.epsilon)
 
-    def add_current_edge_to_list(self, current_edge):
-        assert self.n_loose_edges < self.max_loose_edges
-        self.loose_edges[self.n_loose_edges] = current_edge
+    def add_edge_to_list(self, edge):
+        if self.n_loose_edges >= self.max_loose_edges:
+            return False
+        self.loose_edges[self.n_loose_edges] = edge
         self.n_loose_edges += 1
+        return True
 
     def overwrite_edge_with_last_edge(self, edge_idx):
         self.loose_edges[edge_idx] = self.loose_edges[self.n_loose_edges - 1]
