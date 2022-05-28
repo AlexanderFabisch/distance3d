57ebf3c659efa21e159e7a8ab103313c71e41911
Alexander Fabisch <afabisch@googlemail.com>
2022-05-28 21:23:50 +0200

    Fix unit tests

--- distance3d/test/test_utils.py
+++ distance3d/test/test_utils.py
@@ -5,7 +5,7 @@ from pytest import approx
 
 
 def test_norm_vector():
-    assert_array_almost_equal(norm_vector(np.array([0, 1, 0])), [0, 1, 0])
-    assert_array_almost_equal(norm_vector(np.array([0, 2, 0])), [0, 1, 0])
-    assert_array_almost_equal(norm_vector(np.array([0, 0, 0])), [0, 0, 0])
-    assert approx(1.0) == np.linalg.norm(norm_vector([1, -2.3, 1.3]))
+    assert_array_almost_equal(norm_vector(np.array([0, 1, 0], dtype=float)), [0, 1, 0])
+    assert_array_almost_equal(norm_vector(np.array([0, 2, 0], dtype=float)), [0, 1, 0])
+    assert_array_almost_equal(norm_vector(np.array([0, 0, 0], dtype=float)), [0, 0, 0])
+    assert approx(1.0) == np.linalg.norm(norm_vector(np.array([1, -2.3, 1.3], dtype=float)))
