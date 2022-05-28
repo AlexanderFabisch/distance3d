4ac66d17cf1f74b6734f62433aad7fd35dad7d7e
Alexander Fabisch <afabisch@googlemail.com>
2022-05-28 21:15:21 +0200

    Speed up line to triangle significantly

--- distance3d/utils.py
+++ distance3d/utils.py
@@ -1,10 +1,13 @@
 """Utility functions."""
+import math
+import numba
 import numpy as np
 
 
 MAX_FLOAT = np.finfo(float).max
 
 
+@numba.njit(numba.float64[:](numba.float64[:]), cache=True)
 def norm_vector(v):
     """Normalize vector.
 
@@ -23,3 +26,52 @@ def norm_vector(v):
         return v
 
     return v / norm
+
+
+@numba.njit(numba.types.Tuple(
+    (numba.float64[:], numba.float64[:]))(numba.float64[:]), cache=True)
+def plane_basis_from_normal(plane_normal):
+    """Compute two basis vectors of a plane from the plane's normal vector.
+
+    Note that there are infinitely many solutions because any rotation of the
+    basis vectors about the normal is also a solution. This function
+    deterministically picks one of the solutions.
+
+    The two basis vectors of the plane together with the normal form an
+    orthonormal basis in 3D space and could be used as columns to form a
+    rotation matrix.
+
+    Parameters
+    ----------
+    plane_normal : array-like, shape (3,)
+        Plane normal of unit length.
+
+    Returns
+    -------
+    x_axis : array, shape (3,)
+        x-axis of the plane.
+
+    y_axis : array, shape (3,)
+        y-axis of the plane.
+    """
+    if abs(plane_normal[0]) >= abs(plane_normal[1]):
+        # x or z is the largest magnitude component, swap them
+        length = math.sqrt(
+            plane_normal[0] * plane_normal[0]
+            + plane_normal[2] * plane_normal[2])
+        x_axis = np.array([-plane_normal[2] / length, 0.0,
+                           plane_normal[0] / length])
+        y_axis = np.array([
+            plane_normal[1] * x_axis[2],
+            plane_normal[2] * x_axis[0] - plane_normal[0] * x_axis[2],
+            -plane_normal[1] * x_axis[0]])
+    else:
+        # y or z is the largest magnitude component, swap them
+        length = math.sqrt(plane_normal[1] * plane_normal[1]
+                           + plane_normal[2] * plane_normal[2])
+        x_axis = np.array([0.0, plane_normal[2] / length,
+                           -plane_normal[1] / length])
+        y_axis = np.array([
+            plane_normal[1] * x_axis[2] - plane_normal[2] * x_axis[1],
+            -plane_normal[0] * x_axis[2], plane_normal[0] * x_axis[1]])
+    return x_axis, y_axis
