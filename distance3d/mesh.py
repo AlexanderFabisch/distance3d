196f14b5a831cf34db6dd92e5f9b4136e4dfbcbf
Alexander Fabisch <afabisch@googlemail.com>
2022-05-31 10:17:02 +0200

    Vertex caching and shortcut connections

--- distance3d/mesh.py
+++ distance3d/mesh.py
@@ -7,6 +7,8 @@ class MeshHillClimber:
         self.mesh2origin = mesh2origin
         self.vertices = vertices
 
+        self.first_idx = np.min(triangles)
+
         connections = {}
         for i, j, k in triangles:
             if i not in connections:
@@ -19,12 +21,15 @@ class MeshHillClimber:
             connections[j].update((i, k))
             connections[k].update((i, j))
 
+        self.shortcut_connections = np.array([
+            np.argmax(self.vertices[:, 0]), np.argmax(self.vertices[:, 1]),
+            np.argmax(self.vertices[:, 2]), np.argmin(self.vertices[:, 0]),
+            np.argmin(self.vertices[:, 1]), np.argmin(self.vertices[:, 2])])
+
         self.connections = numba.typed.Dict.empty(numba.int64, numba.int64[:])
         for idx, connected_indices in connections.items():
             self.connections[idx] = np.fromiter(
                 connected_indices, dtype=int, count=len(connected_indices))
-
-        self.first_idx = np.min(triangles)
 
     def update_pose(self, mesh2origin):
         self.mesh2origin = mesh2origin
@@ -34,18 +39,31 @@ class MeshHillClimber:
             self.mesh2origin[:3, :3].T, search_direction)
         idx = hill_climb_mesh_extreme(
             search_direction_in_mesh, self.first_idx, self.vertices,
-            self.connections)
+            self.connections, self.shortcut_connections)
+        self.first_idx = idx  # vertex caching
         return idx, self.mesh2origin[:3, 3] + np.dot(
             self.mesh2origin[:3, :3], self.vertices[idx])
 
 
 @numba.njit(
     numba.int64(numba.float64[:], numba.int64, numba.float64[:, :],
-                numba.types.DictType(numba.int64, numba.int64[:])),
+                numba.types.DictType(numba.int64, numba.int64[:]),
+                numba.optional(numba.int64[:])),
     cache=True)
-def hill_climb_mesh_extreme(search_direction, start_idx, vertices, connections):
+def hill_climb_mesh_extreme(
+        search_direction, start_idx, vertices, connections,
+        shortcut_connections):
     search_direction = np.ascontiguousarray(search_direction)
     best_idx = start_idx
+
+    if shortcut_connections is not None:
+        for connected_idx in shortcut_connections:
+            vertex_diff = np.ascontiguousarray(
+                vertices[connected_idx] - vertices[best_idx])
+            projected_length = search_direction.dot(vertex_diff)
+            if projected_length > 0.0:
+                best_idx = connected_idx
+
     converged = False
     while not converged:
         converged = True
@@ -56,6 +74,7 @@ def hill_climb_mesh_extreme(search_direction, start_idx, vertices, connections):
             if projected_length > 0.0:
                 best_idx = connected_idx
                 converged = False
+
     return best_idx
 
 
