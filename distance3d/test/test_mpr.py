3527907645ce70478a52ff06c649ade41ba09e1f
Alexander Fabisch <afabisch@googlemail.com>
2022-06-04 19:54:02 +0200

    Document and test penetration

--- distance3d/test/test_mpr.py
+++ distance3d/test/test_mpr.py
@@ -1,5 +1,6 @@
 import numpy as np
 from distance3d import colliders, mpr, gjk, epa
+from numpy.testing import assert_array_almost_equal
 
 
 def test_intersection_mpr_vs_gjk():
@@ -17,3 +18,14 @@ def test_intersection_mpr_vs_gjk():
         gjk_intersection = dist < 1e-16
         mpr_intersection = mpr.mpr_intersection(convex1, convex2)
         assert gjk_intersection == mpr_intersection
+
+
+def test_penetration():
+    sphere1 = colliders.Sphere(np.array([0.0, 0.0, 0.0]), 1.0)
+    sphere2 = colliders.Sphere(np.array([0.0, 0.0, 1.0]), 0.5)
+    intersection, depth, penetration_direction, contact_point = mpr.mpr_penetration(
+        sphere1, sphere2)
+    assert intersection
+    assert depth == 0.5
+    assert_array_almost_equal(penetration_direction, [0, 0, 1])
+    assert_array_almost_equal(contact_point, [0, 0, 0.75])
