a5ca6944eed2a613c5d9c305ba3fcf82661e601e
Alexander Fabisch <afabisch@googlemail.com>
2022-06-13 22:41:07 +0200

    Test colliders

--- distance3d/test/test_colliders.py
+++ distance3d/test/test_colliders.py
@@ -16,3 +16,17 @@ def test_box_collider():
     assert_array_almost_equal(
         box.aabb().limits, np.array([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]]))
     assert_array_almost_equal(box.center(), [0, 0, 0])
+
+
+def test_cone_collider():
+    cone = colliders.Cone(np.eye(4), 0.5, 1.0)
+    assert_array_almost_equal(
+        cone.aabb().limits, np.array([[-0.5, 0.5], [-0.5, 0.5], [0.0, 1.0]]))
+    assert_array_almost_equal(cone.center(), [0, 0, 0.5])
+
+
+def test_disk_collider():
+    disk = colliders.Disk(np.zeros(3), 0.5, np.array([0.0, 0.0, 1.0]))
+    assert_array_almost_equal(
+        disk.aabb().limits, np.array([[-0.5, 0.5], [-0.5, 0.5], [0.0, 0.0]]))
+    assert_array_almost_equal(disk.center(), [0, 0, 0])
