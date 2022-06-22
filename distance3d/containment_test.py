944ca9ca9cf7c40204b61cd5dd071e3b543e04e5
Alexander Fabisch <afabisch@googlemail.com>
2022-06-22 10:27:58 +0200

    Point in cylinder

--- distance3d/containment_test.py
+++ distance3d/containment_test.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from .distance import point_to_line_segment
 
@@ -12,3 +13,16 @@ def point_in_capsule(point, capsule2origin, radius, height):
     segment_end = capsule2origin[:, 3] + 0.5 * height * capsule2origin[:3, 2]
     dist = point_to_line_segment(point, segment_start, segment_end)[0]
     return dist <= radius
+
+
+def point_in_cylinder(point, cylinder2origin, radius, length):
+    # signed distance from point to plane of disk
+    diff = point - cylinder2origin[:3, 3]
+    dist_to_plane = diff.dot(cylinder2origin[:3, 2])
+    if abs(dist_to_plane) > 0.5 * length:
+        return False
+
+    # projection of P - C onto plane is Q - C = P - C - dist_to_plane * N
+    diff_in_plane = diff - dist_to_plane * cylinder2origin[:3, 2]
+    dist_in_plane = math.sqrt(diff_in_plane.dot(diff_in_plane))
+    return dist_in_plane <= radius
