2c8b8e0a4de42a0854d2755345e72def752ecbbf
Alexander Fabisch <afabisch@googlemail.com>
2022-06-22 10:36:40 +0200

    Point in disk

--- distance3d/containment_test.py
+++ distance3d/containment_test.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from .distance import point_to_line_segment
+from .utils import EPSILON
 
 
 def point_in_sphere(point, center, radius):
@@ -21,6 +22,19 @@ def point_in_ellipsoid(point, ellipsoid2origin, radii):
     return all(np.abs(point) / radii < 1.0)
 
 
+def point_in_disk(point, center, radius, normal):
+    # signed distance from point to plane of disk
+    diff = point - center
+    dist_to_plane = diff.dot(normal)
+    if abs(dist_to_plane) > 10.0 * EPSILON:
+        return False
+
+    # projection of P - C onto plane is Q - C = P - C - dist_to_plane * N
+    diff_in_plane = diff - dist_to_plane * normal
+    dist_in_plane = diff_in_plane.dot(diff_in_plane)
+    return dist_in_plane <= radius * radius
+
+
 def point_in_cylinder(point, cylinder2origin, radius, length):
     # signed distance from point to plane of disk
     diff = point - cylinder2origin[:3, 3]
@@ -30,8 +44,8 @@ def point_in_cylinder(point, cylinder2origin, radius, length):
 
     # projection of P - C onto plane is Q - C = P - C - dist_to_plane * N
     diff_in_plane = diff - dist_to_plane * cylinder2origin[:3, 2]
-    dist_in_plane = math.sqrt(diff_in_plane.dot(diff_in_plane))
-    return dist_in_plane <= radius
+    dist_in_plane = diff_in_plane.dot(diff_in_plane)
+    return dist_in_plane <= radius * radius
 
 
 def point_in_box(point, box2origin, size):
