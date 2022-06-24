22abd3dbf1f05ef73aea0e753972b0092575dd4b
Alexander Fabisch <afabisch@googlemail.com>
2022-06-24 09:32:46 +0200

    Test capsule

--- distance3d/test/test_containment_test.py
+++ distance3d/test/test_containment_test.py
@@ -12,3 +12,18 @@ def test_points_in_sphere():
     ])
     contained = containment_test.points_in_sphere(points, np.zeros(3), 1.0)
     assert all(contained == [True, True, True, False, False])
+
+
+def test_points_in_capsule():
+    points = np.array([
+        [0.0, 0.0, 0.0],
+        [0.5, 0.0, 0.0],
+        [0.0, -0.5, 0.0],
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.1],
+        [0.0, 0.0, -1.1],
+        [0.5, 0.5, 0.0]
+    ])
+    contained = containment_test.points_in_capsule(points, np.eye(4), 0.5, 1.0)
+    assert all(contained == [True, True, True, True, True, False, False, False])
