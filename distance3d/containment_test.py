402c2e83aaca05a288575f60d2a85dd1265a84bf
Alexander Fabisch <afabisch@googlemail.com>
2022-06-24 16:47:05 +0200

    Add docstrings

--- distance3d/containment_test.py
+++ distance3d/containment_test.py
@@ -3,12 +3,51 @@ from .utils import EPSILON, invert_transform
 
 
 def points_in_sphere(points, center, radius):
+    """Test if points are in sphere.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    center : array, shape (3,)
+        Center of the sphere.
+
+    radius : float
+        Radius of the sphere.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     diff = points - center
     squared_dist = np.sum(diff * diff, axis=1)
     return squared_dist <= radius * radius
 
 
 def points_in_capsule(points, capsule2origin, radius, height):
+    """Test if points are in capsule.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    capsule2origin : array, shape (4, 4)
+        Pose of the capsule.
+
+    radius : float
+        Radius of the capsule.
+
+    height : float
+        Height of the capsule.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     segment_start = capsule2origin[:3, 3] - 0.5 * height * capsule2origin[:3, 2]
     segment_end = capsule2origin[:3, 3] + 0.5 * height * capsule2origin[:3, 2]
     segment_direction = segment_end - segment_start
@@ -24,6 +63,24 @@ def points_in_capsule(points, capsule2origin, radius, height):
 
 
 def points_in_ellipsoid(points, ellipsoid2origin, radii):
+    """Test if points are in ellipsoid.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    ellipsoid2origin : array, shape (4, 4)
+        Pose of the ellipsoid.
+
+    radii : array, shape (3,)
+        Radii of the ellipsoid.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     origin2ellipsoid = invert_transform(ellipsoid2origin)
     points = origin2ellipsoid[:3, 3] + np.dot(points, origin2ellipsoid[:3, :3].T)
     normalized_points = points / radii
@@ -31,6 +88,27 @@ def points_in_ellipsoid(points, ellipsoid2origin, radii):
 
 
 def points_in_disk(points, center, radius, normal):
+    """Test if points are in disk.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    center : array, shape (3,)
+        Center of the disk.
+
+    radius : float
+        Radius of the disk.
+
+    normal : array, shape (3,)
+        Normal to the plane in which the disk lies.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     contained = np.empty(len(points), dtype=bool)
     contained[:] = True
     # signed distance from point to plane of disk
@@ -46,6 +124,27 @@ def points_in_disk(points, center, radius, normal):
 
 
 def points_in_cone(points, cone2origin, radius, height):
+    """Test if points are in cone.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    cone2origin : array, shape (4, 4)
+        Pose of the cone.
+
+    radius : float
+        Radius of the cone.
+
+    height : float
+        Length of the cone.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     contained = np.empty(len(points), dtype=bool)
     contained[:] = True
     half_height = 0.5 * height
@@ -69,6 +168,27 @@ def points_in_cone(points, cone2origin, radius, height):
 
 
 def points_in_cylinder(points, cylinder2origin, radius, length):
+    """Test if points are in cylinder.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    cylinder2origin : array, shape (4, 4)
+        Pose of the cylinder.
+
+    radius : float
+        Radius of the cylinder.
+
+    length : float
+        Length of the cylinder.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     contained = np.empty(len(points), dtype=bool)
     contained[:] = True
     # signed distance from point to plane of disk
@@ -84,12 +204,52 @@ def points_in_cylinder(points, cylinder2origin, radius, length):
 
 
 def points_in_box(points, box2origin, size):
+    """Test if points are in box.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    box2origin : array, shape (4, 4)
+        Pose of the box.
+
+    size : array, shape (3,)
+        Sizes of the box along its axes.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     origin2box = invert_transform(box2origin)
     points = origin2box[:3, 3] + np.dot(points, origin2box[:3, :3].T)
     return np.all(np.abs(points) <= 0.5 * size, axis=1)
 
 
 def points_in_mesh(points, mesh2origin, vertices, triangles):
+    """Test if points are in box.
+
+    Parameters
+    ----------
+    points : array, shape (n_points, 3)
+        Points.
+
+    mesh2origin : array, shape (4, 4)
+        Pose of the mesh.
+
+    vertices : array, shape (n_vertices, 3)
+        Vertices of the mesh.
+
+    triangles : array, shape (n_triangles, 3)
+        Indices of vertices that form triangles of the mesh. Face normals
+        must point outwards.
+
+    Returns
+    -------
+    contained : array, shape (n_points,)
+        Boolean array indicating whether each point is contained or not.
+    """
     origin2mesh = invert_transform(mesh2origin)
     points = origin2mesh[:3, 3] + np.dot(points, origin2mesh[:3, :3].T)
 
