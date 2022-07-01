6f475b43b0bc17dad8061ec593a545fc0c776b41
Alexander Fabisch <afabisch@googlemail.com>
2022-07-01 10:52:58 +0200

    Visualization of ellipse

--- distance3d/visualization.py
+++ distance3d/visualization.py
@@ -57,3 +57,72 @@ class Mesh(pv.Artist):
             List of geometries that can be added to the visualizer.
         """
         return [self.mesh]
+
+
+class Ellipse(pv.Artist):
+    """An ellipse.
+
+    Parameters
+    ----------
+    center : array, shape (3,)
+        Center of ellipse.
+
+    axes : array, shape (2, 3)
+        Axes of ellipse.
+
+    radii : array, shape (2,)
+        Radii of ellipse.
+
+    c : array-like, shape (3,), optional (default: None)
+        Color(s)
+    """
+    def __init__(self, center, axes, radii, c=None):
+        self.mesh = o3d.geometry.TriangleMesh()
+        ellipse2origin = np.eye(4)
+        ellipse2origin[:3, :2] = axes.T
+        ellipse2origin[:3, 2] = np.cross(axes[0], axes[1])
+        ellipse2origin[:3, 3] = center
+        ellipse = np.array([
+            np.hstack(((radii * np.array([np.cos(angle), np.sin(angle)])), (0.0,)))
+            for angle in np.linspace(0, 2 * np.pi, 20)])
+        vertices = np.vstack((np.zeros((1, 3)), ellipse))
+        triangles = np.vstack((
+            np.array([[i, j, 0] for i, j in zip(range(1, len(vertices) - 1),
+                                                range(2, len(vertices)))]),
+            np.array([[0, j, i] for i, j in zip(range(1, len(vertices) - 1),
+                                                range(2, len(vertices)))]),
+        ))
+        self.mesh.vertices = o3d.utility.Vector3dVector(vertices)
+        self.mesh.triangles = o3d.utility.Vector3iVector(triangles)
+        self.mesh.compute_vertex_normals()
+        if c is not None:
+            self.mesh.paint_uniform_color(c)
+        self.ellipse2origin = None
+        self.set_data(ellipse2origin)
+
+    def set_data(self, ellipse2origin):
+        """Update data.
+
+        Parameters
+        ----------
+        ellipse2origin : array-like, shape (4, 4)
+            Pose of the ellipse.
+        """
+        previous_ellipse2origin = self.ellipse2origin
+        if previous_ellipse2origin is None:
+            previous_ellipse2origin = np.eye(4)
+        self.ellipse2origin = ellipse2origin
+
+        self.mesh.transform(np.linalg.inv(previous_ellipse2origin))
+        self.mesh.transform(self.ellipse2origin)
+
+    @property
+    def geometries(self):
+        """Expose geometries.
+
+        Returns
+        -------
+        geometries : list
+            List of geometries that can be added to the visualizer.
+        """
+        return [self.mesh]
