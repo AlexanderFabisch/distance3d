ba3e3c2640b9f5b01bab67360d3a406aa835bbe3
Alexander Fabisch <afabisch@googlemail.com>
2022-07-17 19:35:12 +0200

    Add code to generate icosphere

--- distance3d/mesh.py
+++ distance3d/mesh.py
@@ -1,4 +1,5 @@
 """Tools for convex meshes."""
+import math
 import numba
 import numpy as np
 from scipy.spatial import ConvexHull
@@ -219,3 +220,79 @@ def make_convex_mesh(vertices):
     indices = np.where(angles < HALF_PI)[0]
     triangles[indices] = triangles[indices, ::-1]
     return triangles
+
+
+def make_icosphere(center, radius, order):
+    """Creates an icosphere triangular mesh.
+
+    Source: https://observablehq.com/@mourner/fast-icosphere-mesh
+
+    Parameters
+    ----------
+    center : array, shape (3,)
+        Center of the sphere.
+
+    radius : float
+        Radius of the sphere.
+
+    order : int
+        Number of subdivisions of initial 20 triangles.
+
+    Returns
+    -------
+    vertices : array, shape (n_vertices, 3)
+        Vertices of the mesh.
+
+    triangles : array, shape (n_triangles, 3)
+        Indices of vertices that form triangles of the mesh.
+    """
+    f = (1 + 5 ** 0.5) / 2
+    vertices = np.zeros(((10 * 4 ** order + 2), 3))
+    vertices[:12] = np.array([
+        [-1, f, 0], [1, f, 0], [-1, -f, 0],
+        [1, -f, 0], [0, -1, f], [0, 1, f],
+        [0, -1, -f], [0, 1, -f], [f, 0, -1],
+        [f, 0, 1], [-f, 0, -1], [-f, 0, 1]])
+    triangles = np.array([
+        [0, 11, 5], [0, 5, 1], [0, 1, 7], [0, 7, 10], [0, 10, 11], [11, 10, 2],
+        [5, 11, 4], [1, 5, 9], [7, 1, 8], [10, 7, 6], [3, 9, 4], [3, 4, 2],
+        [3, 2, 6], [3, 6, 8], [3, 8, 9], [9, 8, 1], [4, 9, 5], [2, 4, 11],
+        [6, 2, 10], [8, 6, 7]
+    ])
+    v = 12
+    mid_cache = dict()  # midpoint vertices cache to avoid duplicating shared vertices
+
+    def add_mid_point(a, b, mid_cache, v):
+        # Cantor's pairing function
+        key = math.floor((a + b) * (a + b + 1) / 2) + min(a, b)
+        i = mid_cache.get(key, None)
+        if i is not None:
+            del mid_cache[key]
+            return i, v
+        mid_cache[key] = v
+        vertices[v] = 0.5 * (vertices[a] + vertices[b])
+        i = v
+        v += 1
+        return i, v
+
+    # repeatedly subdivide each triangle into 4 triangles
+    triangles_prev = triangles
+    for i in range(order):
+        triangles = np.empty(
+            (4 * triangles.shape[0], triangles.shape[1]),
+            dtype=int)
+        for k in range(len(triangles_prev)):
+            v1, v2, v3 = triangles_prev[k]
+            a, v = add_mid_point(v1, v2, mid_cache, v)
+            b, v = add_mid_point(v2, v3, mid_cache, v)
+            c, v = add_mid_point(v3, v1, mid_cache, v)
+            t = k * 4
+            triangles[t] = v1, a, c
+            triangles[t + 1] = v2, b, a
+            triangles[t + 2] = v3, c, b
+            triangles[t + 3] = a, b, c
+        triangles_prev = triangles
+
+    vertices /= 1.0 / radius * np.linalg.norm(vertices, axis=1)[:, np.newaxis]
+    vertices += center[np.newaxis]
+    return vertices, triangles
