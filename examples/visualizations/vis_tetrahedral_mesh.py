b1b75c6089e9d86d9d47de4583814c2051457934
Alexander Fabisch <afabisch@googlemail.com>
2022-07-17 14:44:40 +0200

    Search path in example

--- examples/visualizations/vis_tetrahedral_mesh.py
+++ examples/visualizations/vis_tetrahedral_mesh.py
@@ -4,12 +4,21 @@ Visualize Tetrahedral Mesh
 ==========================
 """
 print(__doc__)
+import os
 import numpy as np
 import pytransform3d.visualizer as pv
 from distance3d import io, visualization
 
 
-vertices, tetrahedra = io.load_tetrahedral_mesh("test/data/insole.vtk")
+BASE_DIR = "test/data/"
+data_dir = BASE_DIR
+search_path = ".."
+while (not os.path.exists(data_dir) and
+       os.path.dirname(search_path) != "distance3d"):
+    search_path = os.path.join(search_path, "..")
+    data_dir = os.path.join(search_path, BASE_DIR)
+filename = os.path.join(data_dir, "insole.vtk")
+vertices, tetrahedra = io.load_tetrahedral_mesh(filename)
 
 fig = pv.figure()
 fig.plot_transform(np.eye(4), s=0.1)
