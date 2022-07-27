d037e1463791f8c59286c3d4d9fd5c10a9b8ae8b
Alexander Fabisch <afabisch@googlemail.com>
2022-07-27 23:18:06 +0200

    Test plane projection

--- distance3d/test/test_pressure_field.py
+++ distance3d/test/test_pressure_field.py
@@ -1,6 +1,7 @@
 import numpy as np
-from distance3d import pressure_field
+from distance3d import pressure_field, utils
 from numpy.testing import assert_array_almost_equal
+from pytest import approx
 
 
 def test_intersect_halfplanes():
@@ -18,3 +19,25 @@ def test_intersect_halfplanes():
         [0.5, -0.5]
     ])
     assert_array_almost_equal(polygon, expected_polygon)
+
+
+def test_plane_projection():
+    random_state = np.random.RandomState(3)
+    plane_hnf = random_state.randn(4)
+    plane_hnf = np.array([0.0, 1.0, 1.0, 0.5])
+    plane_hnf[:3] = utils.norm_vector(plane_hnf[:3])
+    x, y = utils.plane_basis_from_normal(plane_hnf[:3])
+    plane2world = np.column_stack((x, y, plane_hnf[:3]))
+    plane_offset = plane_hnf[:3] * plane_hnf[3]
+
+    cart2plane, plane2cart, plane2cart_offset = pressure_field.plane_projection(plane_hnf)
+
+    for _ in range(10):
+        x_plane_cart = random_state.randn(3)
+        x_cart = plane2world.dot(x_plane_cart) + plane_offset
+        x_plane = cart2plane.dot(x_cart)
+        x_cart2 = plane2cart.dot(x_plane) + plane2cart_offset
+        print(x_plane_cart)
+        print(x_cart)
+        print(x_cart2)
+        assert approx(np.linalg.norm(x_cart - x_cart2)) == abs(plane_hnf[:3].dot(x_cart) - plane_hnf[:3].dot(x_cart2))
