63359d65b75b195d6c52b6dd190b6ad9a36e8cf7
Alexander Fabisch <afabisch@googlemail.com>
2022-07-31 23:39:02 +0200

    Fix test

--- distance3d/test/test_pressure_field.py
+++ distance3d/test/test_pressure_field.py
@@ -1,20 +1,21 @@
 import numpy as np
 from distance3d import pressure_field
-from numpy.testing import assert_array_almost_equal
 
 
 def test_intersect_halfplanes():
-    halfplanes = [
-        pressure_field.HalfPlane(np.array([0.5, 0.0]), np.array([-1.0, 0.0])),
-        pressure_field.HalfPlane(np.array([0.0, 0.5]), np.array([0.0, -1.0])),
-        pressure_field.HalfPlane(np.array([-0.5, 0.0]), np.array([1.0, 0.0])),
-        pressure_field.HalfPlane(np.array([0.0, -0.5]), np.array([0.0, 1.0])),
-    ]
-    polygon, _ = pressure_field.intersect_halfplanes(halfplanes)
-    expected_polygon = np.array([
+    halfplanes = np.array([
+        [0.5, 0.0, 0.0, 1.0],
+        [0.0, 0.5, -1.0, 0.0],
+        [-0.5, 0.0, 0.0, -1.0],
+        [0.0, -0.5, 1.0, 0.0]
+    ])
+    polygon = pressure_field.intersect_halfplanes(halfplanes)
+    polygon = [p.tolist() for p in polygon]
+    expected_polygon = [
         [0.5, 0.5],
         [-0.5, 0.5],
         [-0.5, -0.5],
         [0.5, -0.5]
-    ])
-    assert_array_almost_equal(polygon, expected_polygon)
+    ]
+    for p in expected_polygon:
+        assert p in polygon
