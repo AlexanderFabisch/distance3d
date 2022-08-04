163e4e24c211343883bbc39c8c5b2a0bf83afd8d
Alexander Fabisch <alexander.fabisch@dfki.de>
2022-08-04 10:50:42 +0200

    Add unit tests

--- distance3d/test/test_pressure_field.py
+++ distance3d/test/test_pressure_field.py
@@ -1,5 +1,6 @@
 import numpy as np
-from distance3d import pressure_field
+from distance3d import pressure_field, containment
+from numpy.testing import assert_array_almost_equal
 
 
 def test_intersect_halfplanes():
@@ -19,3 +20,24 @@ def test_intersect_halfplanes():
     ]
     for p in expected_polygon:
         assert p in polygon
+
+
+def test_center_of_mass_tetrahedral_mesh():
+    center = np.array([0.0, 0.2, -0.3])
+    vertices, tetrahedra, _ = pressure_field.make_tetrahedral_icosphere(
+        center, 0.2)
+    tetrahedra_points = vertices[tetrahedra]
+    com = pressure_field.center_of_mass_tetrahedral_mesh(tetrahedra_points)
+    assert_array_almost_equal(com, center)
+
+
+def test_tetrahedral_mesh_aabbs():
+    center = np.array([0.0, 0.2, -0.3])
+    vertices, tetrahedra, _ = pressure_field.make_tetrahedral_icosphere(
+        center, 0.2, 2)
+    tetrahedra_points = vertices[tetrahedra]
+    aabbs = pressure_field.tetrahedral_mesh_aabbs(tetrahedra_points)
+    for i in range(len(tetrahedra_points)):
+        mins, maxs = containment.axis_aligned_bounding_box(tetrahedra_points[i])
+        assert_array_almost_equal(mins, aabbs[i, :, 0])
+        assert_array_almost_equal(maxs, aabbs[i, :, 1])
