befbe8c23f86637ec065e77b95513b4fec456e32
Alexander Fabisch <afabisch@googlemail.com>
2022-08-06 00:21:34 +0200

    Extract mesh processing

--- distance3d/pressure_field/__init__.py
+++ distance3d/pressure_field/__init__.py
@@ -8,6 +8,7 @@ from ..utils import (
     plane_basis_from_normal, adjoint_from_transform, EPSILON)
 from ..benchmark import Timer
 from ._tetra_mesh_creation import make_tetrahedral_cube, make_tetrahedral_icosphere
+from ._mesh_processing import tetrahedral_mesh_aabbs, center_of_mass_tetrahedral_mesh
 
 
 class RigidBody:
@@ -588,61 +589,3 @@ def compute_contact_force(
     intersection_com /= total_area
     force_vector = total_force * normal
     return intersection_com, force_vector, total_area
-
-
-def center_of_mass_tetrahedral_mesh(tetrahedra_points):
-    """Compute center of mass of a tetrahedral mesh.
-
-    Assumes uniform density.
-
-    Parameters
-    ----------
-    tetrahedra_points : array, shape (n_tetrahedra, 4, 3)
-        Points that form the tetrahedra.
-
-    Returns
-    -------
-    com : array, shape (3,)
-        Center of mass.
-    """
-    volumes = _tetrahedral_mesh_volumes(tetrahedra_points)
-    centers = tetrahedra_points.mean(axis=1)
-    return np.dot(volumes, centers) / np.sum(volumes)
-
-
-def _tetrahedral_mesh_volumes(tetrahedra_points):
-    """Compute volumes of tetrahedra.
-
-    Parameters
-    ----------
-    tetrahedra_points : array, shape (n_tetrahedra, 4, 3)
-        Points that form the tetrahedra.
-
-    Returns
-    -------
-    volumes : array, shape (n_tetrahedra,)
-        Volumes of tetrahedra.
-    """
-    tetrahedra_edges = tetrahedra_points[:, 1:] - tetrahedra_points[:, np.newaxis, 0]
-    return np.abs(np.sum(
-        np.cross(tetrahedra_edges[:, 0], tetrahedra_edges[:, 1])
-        * tetrahedra_edges[:, 2], axis=1)) / 6.0
-
-
-def tetrahedral_mesh_aabbs(tetrahedra_points):
-    """Compute axis-aligned bounding boxes of tetrahedra.
-
-    Parameters
-    ----------
-    tetrahedra_points : array, shape (n_tetrahedra, 4, 3)
-        Points that form the tetrahedra.
-
-    Returns
-    -------
-    aabbs : array, shape (n_tetrahedra, 3, 2)
-        Axis-aligned bounding boxes of tetrahedra.
-    """
-    mins = np.min(tetrahedra_points, axis=1)
-    maxs = np.max(tetrahedra_points, axis=1)
-    aabbs = np.dstack((mins, maxs))
-    return aabbs
