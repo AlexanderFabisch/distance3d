7b5bf59b5b4b8d2065aaa50320d1c85f02f44da1
Alexander Fabisch <afabisch@googlemail.com>
2022-08-09 23:50:44 +0200

    Test pressure field model

--- distance3d/test/test_pressure_field.py
+++ distance3d/test/test_pressure_field.py
@@ -3,6 +3,45 @@ from distance3d import pressure_field, containment
 from numpy.testing import assert_array_almost_equal
 
 
+def test_contact_forces():
+    mesh12origin = np.eye(4)
+    vertices1, tetrahedra1, potentials1 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.01]), 0.15, 1)
+    mesh22origin = np.eye(4)
+
+    vertices2, tetrahedra2, potentials2 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.4]), 0.15, 1)
+    intersection, wrench12, wrench21 = pressure_field.contact_forces(
+        mesh12origin, vertices1, tetrahedra1, potentials1,
+        mesh22origin, vertices2, tetrahedra2, potentials2)
+    assert not intersection
+    assert_array_almost_equal(wrench12, np.zeros(6))
+    assert_array_almost_equal(wrench21, np.zeros(6))
+
+    vertices2, tetrahedra2, potentials2 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.3]), 0.15, 1)
+    intersection, wrench12, wrench21 = pressure_field.contact_forces(
+        mesh12origin, vertices1, tetrahedra1, potentials1,
+        mesh22origin, vertices2, tetrahedra2, potentials2)
+    assert intersection
+    assert_array_almost_equal(
+        wrench12, np.array([0.0, 0.0, 1.120994e-06, 0.0, 0.0, 0.0]))
+    assert_array_almost_equal(
+        wrench21, np.array([0.0, 0.0, -1.120994e-06, 0.0, 0.0, 0.0]))
+
+    vertices2, tetrahedra2, potentials2 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.15]), 0.15, 1)
+    intersection, wrench12, wrench21 = pressure_field.contact_forces(
+        mesh12origin, vertices1, tetrahedra1, potentials1,
+        mesh22origin, vertices2, tetrahedra2, potentials2)
+    assert intersection
+    assert_array_almost_equal(
+        wrench12, np.array([0.0, 0.0, 0.00166866, 0.0, 0.0, 0.0]))
+    assert_array_almost_equal(
+        wrench21, np.array([0.0, 0.0, -0.00166866, 0.0, 0.0, 0.0]))
+
+
+
 def test_intersect_halfplanes():
     halfplanes = np.array([
         [1.0, 0.0, 0.0, 1.0],
