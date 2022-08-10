511a91532abe3be7e6aaf9d2b63fc660fd3dc7b1
Alexander Fabisch <afabisch@googlemail.com>
2022-08-10 23:58:18 +0200

    Test details

--- distance3d/test/test_pressure_field.py
+++ distance3d/test/test_pressure_field.py
@@ -41,6 +41,37 @@ def test_contact_forces():
         wrench21, np.array([0.0, 0.0, -0.00166866, 0.0, 0.0, 0.0]))
 
 
+def test_contact_forces_and_details():
+    mesh12origin = np.eye(4)
+    vertices1, tetrahedra1, potentials1 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.01]), 0.15, 1)
+    mesh22origin = np.eye(4)
+    vertices2, tetrahedra2, potentials2 = pressure_field.make_tetrahedral_icosphere(
+        np.array([0.0, 0.0, 0.3]), 0.15, 1)
+    intersection, wrench12, wrench21, details = pressure_field.contact_forces(
+        mesh12origin, vertices1, tetrahedra1, potentials1,
+        mesh22origin, vertices2, tetrahedra2, potentials2, return_details=True)
+    assert "contact_polygons" in details
+    assert len(details["contact_polygons"]) == 7
+    assert "contact_polygon_triangles" in details
+    assert len(details["contact_polygon_triangles"]) == 7
+    assert "contact_planes" in details
+    assert len(details["contact_planes"]) == 7
+    assert "intersecting_tetrahedra1" in details
+    assert len(details["intersecting_tetrahedra1"]) == 7
+    assert "intersecting_tetrahedra2" in details
+    assert len(details["intersecting_tetrahedra2"]) == 7
+    assert "contact_coms" in details
+    assert len(details["contact_coms"]) == 7
+    assert "contact_forces" in details
+    assert len(details["contact_forces"]) == 7
+    assert "contact_areas" in details
+    assert len(details["contact_areas"]) == 7
+    assert "pressures" in details
+    assert len(details["pressures"]) == 7
+    assert "contact_point" in details
+
+
 
 def test_intersect_halfplanes():
     halfplanes = np.array([
