08dc09709a60f5868ca0a913cf660a46d2eb1e31
Alexander Fabisch <alexander.fabisch@dfki.de>
2022-09-06 17:49:47 +0200

    Add visualization of potentials

--- bin/compute_potentials.py
+++ bin/compute_potentials.py
@@ -1,6 +1,8 @@
 import argparse
 import json
-from distance3d import io, distance
+import numpy as np
+import pytransform3d.visualizer as pv
+from distance3d import io, distance, visualization
 
 
 def parse_args():
@@ -17,6 +19,8 @@ def parse_args():
                         help="Output file (.json) that will contain a list of "
                              "potentials of the vertices of the tetrahedral "
                              "mesh.")
+    parser.add_argument("--visualize", action="store_true",
+                        help="Visualize result.")
     return parser.parse_args()
 
 
@@ -24,10 +28,13 @@ def main():
     args = parse_args()
 
     surface_vertices, surface_triangles = io.load_mesh(args.triangle_mesh)
-    volume_vertices, _ = io.load_tetrahedral_mesh(args.tetrahedral_mesh)
+    volume_vertices, tetras = io.load_tetrahedral_mesh(args.tetrahedral_mesh)
 
     potentials = compute_potentials(
         surface_triangles, surface_vertices, volume_vertices)
+
+    if args.visualize:
+        visualize(volume_vertices, tetras, potentials)
 
     with open(args.output, "w") as f:
         json.dump(potentials, f)
@@ -41,6 +48,18 @@ def compute_potentials(surface_triangles, surface_vertices, volume_vertices):
             for t in surface_triangles])
         potentials.append(distance_to_surface)
     return potentials
+
+
+def visualize(volume_vertices, tetras, potentials):
+    fig = pv.figure()
+    mesh = visualization.TetraMesh(np.eye(4), volume_vertices, tetras)
+    mesh.add_artist(fig)
+    colors = np.zeros((len(potentials), 3))
+    colors[:, 0] = np.asarray(potentials) / max(potentials)
+    verts = visualization.ColoredVertices(volume_vertices, colors)
+    verts.add_artist(fig)
+    fig.view_init()
+    fig.show()
 
 
 if __name__ == "__main__":
