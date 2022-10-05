af7d3e72a4dda8dce09e4931d7eb5aebd8baf39b
Maarten Behn <46872913+Stroby241@users.noreply.github.com>
2022-10-05 21:34:34 +0200

    minor formatting

--- distance3d/hydroelastic_contact/_broad_phase.py
+++ distance3d/hydroelastic_contact/_broad_phase.py
@@ -4,7 +4,7 @@ import numba
 from ._mesh_processing import tetrahedral_mesh_aabbs
 
 
-def broad_phase_tetrahedra(rigid_body1, rigid_body2, use_aabb_trees = False):
+def broad_phase_tetrahedra(rigid_body1, rigid_body2, use_aabb_trees=False):
     """Broad phase collision detection of tetrahedra.
 
     Parameters
@@ -81,6 +81,6 @@ def _all_aabbs_overlap(aabbs1, aabbs2):
 
 @numba.njit(cache=True)
 def _aabbs_overlap_no_loop(aabb1, aabb2):
-    return aabb1[0][0] < aabb2[0][1] and aabb2[0][0] < aabb1[0][1] \
-           and aabb1[1][0] < aabb2[1][1] and aabb2[1][0] < aabb1[1][1] \
-           and aabb1[2][0] < aabb2[2][1] and aabb2[2][0] < aabb1[2][1]
+    return aabb1[0, 0] < aabb2[0, 1] and aabb2[0, 0] < aabb1[0, 1] \
+           and aabb1[1, 0] < aabb2[1, 1] and aabb2[1, 0] < aabb1[1, 1] \
+           and aabb1[2, 0] < aabb2[2, 1] and aabb2[2, 0] < aabb1[2, 1]
