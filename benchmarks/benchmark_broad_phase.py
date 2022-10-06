a9b43507b70daf489e4ec3fe27d271e07529d57e
Maarten Behn <maarten.behn@gmail.com>
2022-10-06 23:38:49 +0200

    tree performance test

--- benchmarks/benchmark_broad_phase.py
+++ benchmarks/benchmark_broad_phase.py
@@ -1,4 +1,6 @@
 import timeit
+
+import aabbtree
 import numpy as np
 from functools import partial
 from distance3d import hydroelastic_contact
@@ -6,10 +8,10 @@ from distance3d import hydroelastic_contact
 random_state = np.random.RandomState(0)
 
 
-def create_random_spheres(random_state):
+def create_random_spheres(random_state, size):
     p = random_state.randn(3)
-    rigid_body1 = hydroelastic_contact.RigidBody.make_sphere(0 * np.ones(3), 0.15, 4)
-    rigid_body2 = hydroelastic_contact.RigidBody.make_sphere(p, 0.15, 4)
+    rigid_body1 = hydroelastic_contact.RigidBody.make_sphere(0 * np.ones(3), 0.15, size)
+    rigid_body2 = hydroelastic_contact.RigidBody.make_sphere(p, 0.15, size)
     rigid_body1.express_in(rigid_body2.body2origin_)
 
     aabbs1 = hydroelastic_contact.tetrahedral_mesh_aabbs(rigid_body1.tetrahedra_points)
@@ -18,37 +20,47 @@ def create_random_spheres(random_state):
     return aabbs1, aabbs2
 
 
-def brute_force_aabbs(random_state, aabbs1, aabbs2):
+def brute_force_aabbs(aabbs1, aabbs2):
     hydroelastic_contact._all_aabbs_overlap(aabbs1, aabbs2)
 
-def aabb_tree(random_state, aabbs1, aabbs2):
+def aabb_tree(aabbs1, aabbs2):
     root1, nodes1, aabbs1 = hydroelastic_contact.new_tree_from_aabbs(aabbs1)
     root2, nodes2, aabbs2 = hydroelastic_contact.new_tree_from_aabbs(aabbs2)
 
     hydroelastic_contact.query_overlap_of_other_tree(root1, nodes1, aabbs1, root2, nodes2, aabbs2)
 
 
-aabbs1, aabbs2 = create_random_spheres(random_state)
+def old_aabb_tree(aabbs1, aabbs2):
+    tree2 = aabbtree.AABBTree()
+    for j, aabb in enumerate(aabbs2):
+        tree2.add(aabbtree.AABB(aabb), j)
+    broad_tetrahedra1 = []
+    broad_tetrahedra2 = []
+    for i, aabb in enumerate(aabbs1):
+        new_indices2 = tree2.overlap_values(aabbtree.AABB(aabb), method='DFS', closed=False, unique=False)
+        broad_tetrahedra2.extend(new_indices2)
+        broad_tetrahedra1.extend([i] * len(new_indices2))
+
+
+aabbs1, aabbs2 = create_random_spheres(random_state, 2)
 
 repeat = 5
 number = 5
 
 
-times = timeit.repeat(partial(brute_force_aabbs, random_state=random_state, aabbs1=aabbs1, aabbs2=aabbs2), repeat=repeat, number=number)
-print(f"Brute Force Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
+# times = timeit.repeat(partial(brute_force_aabbs, aabbs1=aabbs1, aabbs2=aabbs2), repeat=repeat, number=number)
+# print(f"Brute Force Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
 
-root1, nodes1, aabbs1 = hydroelastic_contact.new_tree_from_aabbs(aabbs1)
-root2, nodes2, aabbs2 = hydroelastic_contact.new_tree_from_aabbs(aabbs2)
+# root1, nodes1, aabbs1 = hydroelastic_contact.new_tree_from_aabbs(aabbs1)# root2, nodes2, aabbs2 = hydroelastic_contact.new_tree_from_aabbs(aabbs2)
 
-times = timeit.repeat(partial(hydroelastic_contact.query_overlap_of_other_tree, root1, nodes1, aabbs1, root2, nodes2, aabbs2),repeat=repeat, number=number)
-print(f"AABB Trees No Creation Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
+# times = timeit.repeat(partial(hydroelastic_contact.query_overlap_of_other_tree, root1, nodes1, aabbs1, root2, nodes2, aabbs2),repeat=repeat, number=number)
+# print(f"AABB Trees No Creation Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
 
-times = timeit.repeat(partial(aabb_tree, random_state=random_state, aabbs1=aabbs1, aabbs2=aabbs2 ),repeat=repeat, number=number)
+times = timeit.repeat(partial(aabb_tree, aabbs1=aabbs1, aabbs2=aabbs2), repeat=repeat, number=number)
 print(f"AABB Trees Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
 
-# times = timeit.repeat(partial(random_broad_phase_collisions, random_state=random_state, use_aabb_trees=True, use_old=True), repeat=repeat, number=number)
+# times = timeit.repeat(partial(old_aabb_tree, aabbs1=aabbs1, aabbs2=aabbs2), repeat=repeat, number=number)
 # print(f"Old AABB Trees Mean: {np.mean(times):.5f}; Std. dev.: {np.std(times):.5f}")
-
 
 
 """
---
+++ benchmarks/benchmark_broad_phase_plot.py
@@ -0,0 +1,41 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+
+"""
+Brute Force Mean: 0.00139; Std. dev.: 0.00268
+AABB Trees No Creation Mean: 0.00265; Std. dev.: 0.00526
+AABB Trees Mean: 0.03471; Std. dev.: 0.00207
+Old AABB Trees Mean: 0.28774; Std. dev.: 0.00624
+
+Brute Force Mean: 0.00200; Std. dev.: 0.00282
+AABB Trees No Creation Mean: 0.00239; Std. dev.: 0.00476
+AABB Trees Mean: 0.19004; Std. dev.: 0.00541
+Old AABB Trees Mean: 1.60006; Std. dev.: 0.01856
+
+Brute Force Mean: 0.01090; Std. dev.: 0.00295
+AABB Trees No Creation Mean: 0.00279; Std. dev.: 0.00552
+AABB Trees Mean: 0.94646; Std. dev.: 0.00822
+
+Brute Force Mean: 0.15937; Std. dev.: 0.00502
+AABB Trees No Creation Mean: 0.00247; Std. dev.: 0.00490
+AABB Trees Mean: 4.70398; Std. dev.: 0.06692
+
+Brute Force Mean: 2.37237; Std. dev.: 0.02910
+AABB Trees No Creation Mean: 0.00232; Std. dev.: 0.00460
+AABB Trees Mean: 77.21539; Std. dev.: 1.11606
+
+Brute Force Mean: 40.64899; Std. dev.: 1.14510
+AABB Trees No Creation Mean: 0.00238; Std. dev.: 0.00471
+
+AABB Trees No Creation Mean: 0.00243; Std. dev.: 0.00483
+
+"""
+
+plt.plot([0.00139, 0.00200, 0.01090, 0.15937, 2.37237, 40.64899], label='Brute Force')
+plt.plot([0.00265, 0.00239, 0.00279, 0.00247, 0.00232, 0.00244, 0.00310], label='AABB Trees No Creation')
+plt.plot([0.03471, 0.19004, 0.94646, 4.70398, 77.21539], label='AABB Trees')
+plt.plot([0.2877, 1.60006, 11.77460], label='Old AABB Trees')
+plt.show()
